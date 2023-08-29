b8fe64a7da08be91389e910d62b5fe82a612d6f2
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-29 12:11:25 -0300

    continuação...

---
+++ node/rota.js
@@ -0,0 +1,20 @@
+const http = require('http');
+var server = http.createServer(function(request, response){
+    response.writeHead("200", {"Content-Type": "text/html"});
+    if(request.url == "/"){
+        response.write("<h1>Página Principal</h1>")
+    }
+    else if(request.url == "/bemvindo"){
+        response.write("<h1>Bem vindo!!!</h1>")
+    }
+    else {
+        response.write("<h1>Página não encontrada</h1>")
+    }
+
+    response.end();
+});
+
+const port = 3000;
+server.listen(port, () =>{
+    console.log(`Servidores executado em http://localhost:${port}`)
+})
